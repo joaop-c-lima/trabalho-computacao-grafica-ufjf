7c624cf351476be983f7414e097b0e443c156edc
giovannikvothe <reheal1504@gmail.com>
2023-04-06 10:49:26 -0300

    Mira atualizada

--- T1/aim.js
+++ T1/aim.js
@@ -3,22 +3,31 @@ import * as THREE from  'three';
 export function createAim(){
     //Creating a grid of points
     var vertices = [];
-    vertices.push( new THREE.Vector3( 0, 1.5, 0 ) );
-    vertices.push( new THREE.Vector3( 0, -1.5, 0 ) );
-    vertices.push( new THREE.Vector3( 1.5, 0, 0 ) );
-    vertices.push( new THREE.Vector3( -1.5, 0, 0 ) );
+    vertices.push( new THREE.Vector3( 0, 0.5, 0 ) );
+    vertices.push( new THREE.Vector3( 0, -0.5, 0 ) );
+    vertices.push( new THREE.Vector3( 0, 0, 0 ) );
+    vertices.push( new THREE.Vector3( 0.5, 0, 0 ) );
+    vertices.push( new THREE.Vector3( -0.5, 0, 0 ) );
 
-    vertices.push( new THREE.Vector3( 0, 1.5, -1 ) );
-    vertices.push( new THREE.Vector3( 1.5, 0, -1 ) );
-    vertices.push( new THREE.Vector3( -1.5, 0, -1 ) );
-    vertices.push( new THREE.Vector3( 0, -1.5, -1 ) );
+    var vertices2 = [];
+    vertices2.push( new THREE.Vector3( 0, 0.5, 0 ) );
+    vertices2.push( new THREE.Vector3( 0.5, 0, 0 ) );
+    vertices2.push( new THREE.Vector3( 0, -0.5, 0 ) );
+    vertices2.push( new THREE.Vector3( -0.5, 0, 0 ) );
+    vertices2.push( new THREE.Vector3( 0, 0.5, 0 ) );
     
     //Transforming in a Geometry
     var geometry = new THREE.BufferGeometry().setFromPoints(vertices);
+    var geometry2 = new THREE.BufferGeometry().setFromPoints(vertices2);
     //Setting material
     var material = new THREE.LineBasicMaterial( { color: 0xffffff } );
     //Drawing the geometry
     var crosshair = new THREE.Line( geometry, material );
-    crosshair.position.set(0, 30, 5)
+    var crosshair2 = new THREE.Line( geometry2, material );
+
+    crosshair.position.set(3, 3, -3)
+    crosshair2.position.set(0, 0, 0.75)
+
+    crosshair.add(crosshair2)
     return crosshair;
 }
