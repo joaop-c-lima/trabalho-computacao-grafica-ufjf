e53927408ef34296ee2f909b4a2b96f03e46941e
giovannikvothe <reheal1504@gmail.com>
2023-04-11 00:03:03 -0300

    Envio Final T1

--- T1/main.js
+++ T1/main.js
@@ -29,7 +29,7 @@ document.body.style.cursor = 'none';
 //Pointer Lock
 const canvas = document.querySelector("canvas");
 canvas.addEventListener("click", async () => {
-  canvas.requestPointerLock({unadjustedMovement: true});
+  canvas.requestPointerLock();
 });
 
 //Create plane
